7024a8cc464be8950d73e1e2fbecd14ddc98d241
kzw200015 <kzw200015@gmail.com>
2022-03-22 16:34:30 +0800

    添加表结构

--- src/main/java/cc/jktu/gis/model/mapper/PoliceMapper.java
+++ src/main/java/cc/jktu/gis/model/mapper/PoliceMapper.java
@@ -1,9 +1,10 @@
 package cc.jktu.gis.model.mapper;
 
+import cc.jktu.gis.model.entity.PoliceEntity;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
-public interface PoliceMapper extends BaseMapper<PoliceMapper> {
+public interface PoliceMapper extends BaseMapper<PoliceEntity> {
 
 }
